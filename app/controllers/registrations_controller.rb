96e81c844d62e2374efe6d9c81e0ee8b1d746704
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-27 04:11:08 -0500

    feat: add core authentication controllers
    - SessionsController for sign in/out
    - RegistrationsController for user signup
    - PasswordsController for password resets

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,32 @@
+class RegistrationsController < ApplicationController
+  skip_before_action :authenticate
+  skip_before_action :ensure_verified
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @account = Account.create!
+    @user = @account.users.build(user_params)
+
+    if @user.save
+      session_record = @user.sessions.create!
+      cookies.signed.permanent[:session_token] = { value: session_record.id, httponly: true }
+
+      send_email_verification
+      redirect_to root_path, notice: "Welcome! You have signed up successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+
+    def send_email_verification
+      UserMailer.with(user: @user).email_verification.deliver_later
+    end
+end
