85dfc9642e8d0b6513dcfa9a45bc064b7fb77222
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-27 04:11:46 -0500

    feat: add invitation and masquerading controllers
    - InvitationsController for team member invites
    - MasqueradesController for admin user impersonation
    - Fixed multitenancy integration and admin authorization

---
+++ app/controllers/masquerades_controller.rb
@@ -0,0 +1,25 @@
+class MasqueradesController < ApplicationController
+  before_action :authorize_admin
+  before_action :set_user
+
+  def create
+    session_record = @user.sessions.create!(
+      user_agent: request.user_agent,
+      ip_address: request.remote_ip
+    )
+    cookies.signed.permanent[:session_token] = { value: session_record.id, httponly: true }
+
+    redirect_to root_path, notice: "Masquerading as #{@user.email}"
+  end
+
+  private
+    def set_user
+      @user = Current.user.account.users.find(params[:user_id])
+    end
+
+    def authorize_admin
+      unless Current.user == Current.user.account.users.first || Rails.env.development?
+        redirect_to(root_path, alert: "Access denied")
+      end
+    end
+end
