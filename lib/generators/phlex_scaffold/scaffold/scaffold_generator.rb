6c0d135c487e5551c88fe18b89c8d178ac2eb618
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-28 12:01:10 -0500

    fix: Correct template engine generator structure
    - Move phlex_scaffold generator to proper directory structure
    - Follow Rails template engine naming convention
    - Fix generator namespace to match erb:scaffold pattern

---
+++ lib/generators/phlex_scaffold/scaffold/scaffold_generator.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+# This generator allows Phlex to be used as a template engine for scaffolds
+# when configured in config/application.rb with:
+#   config.generators.template_engine :phlex_scaffold
+
+module PhlexScaffold
+  module Generators
+    class ScaffoldGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path("../../phlex/scaffold/templates", __dir__)
+
+      argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
+
+      def create_views
+        %w[index show new edit].each do |view|
+          @view_name = view
+          template "#{view}.rb.erb", File.join("app/views", class_path, plural_file_name, "#{view}.rb")
+        end
+
+        # Create the form partial
+        @view_name = "form"
+        template "_form.rb.erb", File.join("app/views", class_path, plural_file_name, "_form.rb")
+      end
+
+      private
+
+      def model_name
+        @model_name ||= class_name.classify
+      end
+
+      def plural_table_name
+        @plural_table_name ||= model_name.tableize
+      end
+
+      def singular_table_name
+        @singular_table_name ||= model_name.tableize.singularize
+      end
+
+      def instance_name
+        "@#{singular_table_name}"
+      end
+
+      def plural_instance_name
+        "@#{plural_table_name}"
+      end
+
+      def attributes_list
+        return [] if attributes.empty?
+        
+        attributes.map do |attr|
+          { name: attr.name, type: attr.type.to_s }
+        end
+      end
+
+      def display_attributes
+        return [{ name: 'name', type: 'string' }] if attributes_list.empty?
+        
+        # Filter out timestamps and id
+        attributes_list.reject { |attr| %w[id created_at updated_at].include?(attr[:name]) }
+      end
+
+      def form_attributes
+        display_attributes
+      end
+    end
+  end
+end 
