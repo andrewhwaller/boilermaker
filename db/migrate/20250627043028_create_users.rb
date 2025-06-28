20edccc86edf134155191f3797292712c4f6982d
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-28 12:42:23 -0500

    Fix migration

--- db/migrate/20250627043028_create_users.rb
+++ db/migrate/20250627043028_create_users.rb
@@ -1,1 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.boolean :verified, default: false
+      t.boolean :otp_required_for_sign_in, default: false
+      t.string :otp_secret
+      t.references :account, null: false, foreign_key: true
 
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
