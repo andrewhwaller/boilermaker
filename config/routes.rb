a430682dc4c77aaf27cb9bcb11545fbf76b38c14
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-06-27 20:19:08 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get  "sign_in", to: "sessions#new"
 
   # Session management
-  resources :sessions, only: [ :index, :show, :destroy ]
+  resources :sessions, only: [ :show, :destroy ]
 
   # Password management
   resource :password, only: [ :edit, :update ]
