d11a872bc7245388f04311e5236f1d2acf27279c
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-05 19:00:44 -0500

    refactor(tokens): standardize on sid param for signed tokens
    - Email verification, password reset, and invitations now use sid
    - Controller and mailer templates updated accordingly

--- app/views/user_mailer/password_reset.rb
+++ app/views/user_mailer/password_reset.rb
@@ -13,7 +13,7 @@ module Views
           p { "Someone requested a password reset for your account. If this was you, you can reset your password through the link below:" }
 
           div(class: "button-container") do
-            link_to "Reset Password", edit_identity_password_reset_url(token: @signed_id), class: "button"
+            link_to "Reset Password", edit_identity_password_reset_url(sid: @signed_id), class: "button"
           end
 
           p { "If you didn't request this, you can safely ignore this email. Your password won't be changed until you access the link above and create a new one." }
