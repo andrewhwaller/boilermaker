e1523507829f5570503ae13d9713d7ab087749f8
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-07 09:54:07 -0500

    Remove max users validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,6 @@ class Account < ApplicationRecord
   has_many :users, dependent: :destroy
 
   validates :name, presence: true
-  validate :max_users_limit, on: :create
 
   # Create a default account if personal accounts are enabled
   def self.create_default_for_user(user_email)
@@ -10,24 +9,5 @@ class Account < ApplicationRecord
 
     default_name = Boilermaker.config.get("accounts.default_account_name") || "Personal"
     create!(name: "#{default_name} (#{user_email})")
-  end
-
-  # Check if this account can accept more users
-  def can_add_user?
-    return true unless Boilermaker.config.multi_tenant?
-
-    max_users = Boilermaker.config.max_users_per_account
-    users.count < max_users
-  end
-
-  private
-
-  def max_users_limit
-    return unless Boilermaker.config.multi_tenant?
-
-    max_users = Boilermaker.config.max_users_per_account
-    if users.count >= max_users
-      errors.add(:base, "Account has reached the maximum number of users (#{max_users})")
-    end
   end
 end
