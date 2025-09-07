3cd50b510b1e606105aecdaeb8e58261569603f5
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-06 20:40:45 -0500

    Add theme_name attribute

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,6 +1,6 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
-  attribute :user_agent, :ip_address
+  attribute :user_agent, :ip_address, :theme_name
 
   delegate :user, to: :session, allow_nil: true
 end
