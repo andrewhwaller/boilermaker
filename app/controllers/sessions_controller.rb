410ad910195c83e45e03c2581eafc51c473a7458
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-24 22:10:59 -0500

    Update controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,6 +34,10 @@ class SessionsController < ApplicationController
 
   private
     def set_session
-      @session = Current.user.sessions.find(params[:id])
+      if params[:id] == "current"
+        @session = Current.session
+      else
+        @session = Current.user.sessions.find(params[:id])
+      end
     end
 end
