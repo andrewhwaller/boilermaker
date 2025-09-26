c724cdf6f51125802f92649a9c17b5ffd201c515
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-26 15:13:10 -0500

    Update dashboard controller

--- app/controllers/account/dashboards_controller.rb
+++ app/controllers/account/dashboards_controller.rb
@@ -4,8 +4,8 @@ class Account::DashboardsController < Account::BaseController
 
     render Views::Account::Dashboards::Show.new(
       account: account,
-      users: account.users.verified.order(:last_name, :first_name).limit(5),
-      invitations: account.users.unverified.order(created_at: :desc).limit(5)
+      users: account.users.verified.order(:last_name, :first_name),
+      invitations: account.users.unverified.order(created_at: :desc)
     )
   end
 end
