97653d85b42562695f8bc4ba21ea44b64d5bab59
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-09-26 15:13:21 -0500

    Update users controller

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,5 +1,5 @@
 class Account::UsersController < Account::BaseController
-  before_action :set_user, only: [ :show, :edit, :update ]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
 
   def index
     redirect_to account_invitations_path
@@ -14,15 +14,30 @@ class Account::UsersController < Account::BaseController
   end
 
   def update
-    admin_flag = params.dig(:user, :admin) == "1"
-
-    if @user.update(user_params)
-      membership = AccountMembership.find_or_create_by!(user: @user, account: Current.user.account)
-      new_roles = membership.roles.merge("member" => true, "admin" => admin_flag)
-      membership.update!(roles: new_roles)
-      redirect_to account_user_path(@user), notice: "User updated successfully."
+    # Handle role updates separately from user updates
+    if params[:role].present?
+      update_membership_role
+    elsif params[:user].present?
+      update_user_attributes
     else
-      render Views::Account::Users::Edit.new(user: @user), status: :unprocessable_entity
+      head :bad_request
+    end
+  end
+
+  def destroy
+    # Prevent users from removing themselves
+    if @user == Current.user
+      redirect_to account_path, alert: "You cannot remove yourself from the account"
+      return
+    end
+
+    # Find and destroy the account membership
+    membership = AccountMembership.find_by(user: @user, account: Current.user.account)
+
+    if membership&.destroy
+      redirect_to account_path, notice: "#{@user.email} has been removed from the account"
+    else
+      redirect_to account_path, alert: "Failed to remove user from account"
     end
   end
 
@@ -32,7 +47,44 @@ class Account::UsersController < Account::BaseController
     @user = Current.user.account.users.find(params[:id])
   end
 
+  def update_membership_role
+    membership = AccountMembership.find_or_create_by!(user: @user, account: Current.user.account)
+    role_param = Array(params[:role]).last
+    admin_flag = role_param == "admin"
+    new_roles = membership.roles.merge("member" => true, "admin" => admin_flag)
+
+    if membership.update(roles: new_roles)
+      respond_to do |format|
+        format.turbo_stream { head :ok }
+        format.html { redirect_to account_user_path(@user) }
+        format.json { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { head :unprocessable_entity }
+        format.html { redirect_to account_user_path(@user), alert: "Failed to update role" }
+        format.json { head :unprocessable_entity }
+      end
+    end
+  end
+
+  def update_user_attributes
+    if @user.update(user_params)
+      respond_to do |format|
+        format.turbo_stream { head :ok }
+        format.html { redirect_to account_user_path(@user) }
+        format.json { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { head :unprocessable_entity }
+        format.html { redirect_to account_user_path(@user), alert: "Failed to update user" }
+        format.json { head :unprocessable_entity }
+      end
+    end
+  end
+
   def user_params
-    params.require(:user).permit(:email, :verified)
+    params.require(:user).permit(:email)
   end
 end
