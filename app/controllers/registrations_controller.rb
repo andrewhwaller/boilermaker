7c8597f9c973e258d90e8b4e17dc2d7c6e1f8009
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-02 14:29:37 -0500

    Fix user registration strong parameters
    - Change user_params from params.permit to params.require(:user).permit
    - Form sends nested user params, need to require :user key
    - Fixes 422 error where email was NULL due to unpermitted params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -53,7 +53,7 @@ class RegistrationsController < ApplicationController
 
   private
     def user_params
-      params.permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
     def send_email_verification
