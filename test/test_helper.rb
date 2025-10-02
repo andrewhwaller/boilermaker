7967841553bb4af998be47fa5b3ce566794cbded
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-02 04:40:16 -0500

    Update test helpers for account context

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/mock"
+require "active_job/test_helper"
 
 module ActiveSupport
   class TestCase
+    include ActiveJob::TestHelper
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
@@ -16,8 +19,12 @@ end
 
 module ActionDispatch
   class IntegrationTest
-    def sign_in_as(user)
-      post sign_in_url, params: { email: user.email, password: "Secret1*3*5*" }
+    def sign_in_as(user, account = nil)
+      post sign_in_url, params: {email: user.email, password: "Secret1*3*5*"}
+      if account
+        session = user.sessions.last
+        session.update!(account: account) if session
+      end
       user
     end
   end
