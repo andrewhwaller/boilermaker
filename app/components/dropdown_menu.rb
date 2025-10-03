f7b07468c4a13a415c5024418edeb1a4a0cc3ba4
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-03 10:37:10 -0500

    Enhance DropdownMenu component with alignment and customization options
    Changes:
    - Added align parameter (:end, :top, :bottom, :start)
    - Support custom trigger content via lambda/proc
    - Allow custom CSS classes for trigger and menu
    - Support custom data attributes
    - Add helper methods for class merging
    - Maintain backward compatibility with trigger_text parameter

--- app/components/dropdown_menu.rb
+++ app/components/dropdown_menu.rb
@@ -1,12 +1,37 @@
 # frozen_string_literal: true
 
 class Components::DropdownMenu < Components::Base
-  def initialize(trigger_text:)
+  DEFAULT_TRIGGER_OPTIONS = {
+    type: "button",
+    class: "btn btn-ghost font-medium",
+    data: { action: nil },
+    tabindex: 0,
+    role: "button"
+  }.freeze
+
+  DEFAULT_MENU_OPTIONS = {
+    class: "dropdown-content p-0 menu menu-sm bg-base-200 border border-base-300 rounded-box z-50 w-48",
+    tabindex: 0,
+    data: {}
+  }.freeze
+
+  def initialize(trigger_text: nil, align: :end, trigger: {}, menu: {}, **attributes)
     @trigger_text = trigger_text
+    @align = align
+    trigger_options = trigger.dup
+    @trigger_content = trigger_options.delete(:content)
+    @trigger_options = build_trigger_options(trigger_options)
+    @menu_options = build_menu_options(menu)
+    @attributes = attributes
   end
 
   def view_template(&block)
-    div(class: "dropdown dropdown-end", data: { controller: "dropdown" }, tabindex: 0) do
+    div(
+      class: css_classes("dropdown", alignment_class),
+      data: { controller: "dropdown" }.merge(@attributes.fetch(:data, {})),
+      tabindex: @attributes.fetch(:tabindex, 0),
+      **filtered_attributes(:class, :data, :tabindex)
+    ) do
       trigger
       menu(&block)
     end
@@ -14,26 +39,42 @@ class Components::DropdownMenu < Components::Base
 
   private
 
+  def alignment_class
+    case @align
+    when :end then "dropdown-end"
+    when :top then "dropdown-top"
+    when :bottom then "dropdown-bottom"
+    when :start then nil
+    else
+      @align
+    end
+  end
+
   def trigger
-    button(
-      type: "button",
-      class: "btn btn-ghost",
-      data: { action: nil },
-      tabindex: 0,
-      role: "button"
-    ) do
-      @trigger_text if @trigger_text
+    options = @trigger_options.dup
+    button_class = options.delete(:class)
+    data_attributes = options.delete(:data) || {}
+
+    button(**options.merge(class: button_class, data: data_attributes)) do
+      if @trigger_content.respond_to?(:call)
+        instance_exec(&@trigger_content)
+      elsif @trigger_text
+        plain(@trigger_text)
+      end
+
       chevron
     end
   end
 
   def menu(&block)
-    ul(
-      class: "dropdown-content menu menu-sm gap-1 p-2 shadow bg-base-200 border border-base-300 rounded-box z-50 w-48 mt-2",
-      tabindex: 0,
-      data: { dropdown_target: "menu" },
-      &block
-    )
+    return unless block
+
+    options = @menu_options.dup
+    list_class = options.delete(:class)
+    data_attributes = { dropdown_target: "menu" }.merge(options.delete(:data) || {})
+    tabindex = options.delete(:tabindex)
+
+    ul(class: list_class, data: data_attributes, tabindex: tabindex, **options, &block)
   end
 
   def chevron
@@ -50,5 +91,25 @@ class Components::DropdownMenu < Components::Base
         d: "M19 9l-7 7-7-7"
       )
     end
+  end
+
+  def build_trigger_options(overrides)
+    defaults = DEFAULT_TRIGGER_OPTIONS.deep_dup
+    defaults[:class] = merge_classes(defaults[:class], overrides.delete(:class))
+    defaults[:data] = defaults[:data].merge(overrides.delete(:data) || {})
+    defaults.merge!(overrides)
+    defaults
+  end
+
+  def build_menu_options(overrides)
+    defaults = DEFAULT_MENU_OPTIONS.deep_dup
+    defaults[:class] = merge_classes(defaults[:class], overrides.delete(:class))
+    defaults[:data] = defaults[:data].merge(overrides.delete(:data) || {})
+    defaults.merge!(overrides)
+    defaults
+  end
+
+  def merge_classes(*values)
+    values.flatten.compact.map(&:to_s).map(&:strip).reject(&:empty?).join(" ")
   end
 end
