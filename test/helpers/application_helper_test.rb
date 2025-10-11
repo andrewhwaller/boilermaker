2fc5b3d08f147ffab1b3a4a2303b611163cb9e6c
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2025-10-10 21:16:14 -0500

    Clean up application_helper_test file

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,117 +3,20 @@
 require "test_helper"
 
 class ApplicationHelperTest < ActionView::TestCase
-  def setup
-    @original_data = Boilermaker::Config.instance_variable_get(:@data)
+  test "page_title returns app name when no title provided" do
+    assert_equal Boilermaker.config.app_name, page_title
   end
 
-  def teardown
-    Boilermaker::Config.instance_variable_set(:@data, @original_data)
+  test "page_title includes custom title with app name" do
+    title = page_title("Dashboard")
+    assert_includes title, "Dashboard"
+    assert_includes title, Boilermaker.config.app_name
   end
 
-  test "font_stylesheet_link_tag returns nil for CommitMono" do
-    stub_font_config("CommitMono")
-
-    result = font_stylesheet_link_tag
-
-    assert_nil result
-  end
-
-  test "font_stylesheet_link_tag returns link tags for Google Fonts" do
-    stub_font_config("Inter")
-
-    result = font_stylesheet_link_tag
-
-    assert_not_nil result
-    assert_includes result, "fonts.googleapis.com"
-    assert_includes result, "fonts.gstatic.com"
-    assert_includes result, "Inter"
-    assert_includes result, 'rel="preconnect"'
-    assert_includes result, 'rel="stylesheet"'
-    assert_includes result, 'crossorigin="anonymous"'
-  end
-
-  test "font_stylesheet_link_tag returns link tags for Monaspace" do
-    stub_font_config("Monaspace Neon")
-
-    result = font_stylesheet_link_tag
-
-    assert_not_nil result
-    assert_includes result, "cdn.jsdelivr.net"
-    assert_includes result, "@font-face"
-    assert_includes result, "<style>"
-    assert_includes result, 'rel="preload"'
-    assert_not_includes result, "/fonts/monaspace-neon.css"
-  end
-
-  test "app_font_family returns correct stack for CommitMono" do
-    stub_font_config("CommitMono")
-
-    result = app_font_family
-
-    assert_equal '"CommitMonoIndustrial", monospace', result
-  end
-
-  test "app_font_family returns correct stack for Inter" do
-    stub_font_config("Inter")
-
-    result = app_font_family
-
-    assert_includes result, "Inter"
-    assert_includes result, "sans-serif"
-  end
-
-  test "app_font_family returns correct stack for Monaspace" do
-    stub_font_config("Monaspace Neon")
-
-    result = app_font_family
-
-    assert_includes result, "Monaspace Neon"
-    assert_includes result, "monospace"
-  end
-
-  test "app_text_transform defaults to uppercase" do
-    Boilermaker::Config.instance_variable_set(:@data, {})
-
-    assert_equal "uppercase", app_text_transform
-  end
-
-  test "app_text_transform reflects config toggle" do
-    Boilermaker::Config.instance_variable_set(:@data, {
-      "ui" => {
-        "typography" => { "uppercase" => false }
-      }
-    })
-
-    assert_equal "none", app_text_transform
-  end
-
-  test "app_base_font_size defaults to multiplier 1" do
-    Boilermaker::Config.instance_variable_set(:@data, {})
-
-    assert_in_delta 1.0, app_base_font_size
-  end
-
-  test "app_base_font_size reflects configured scale" do
-    Boilermaker::Config.instance_variable_set(:@data, {
-      "ui" => {
-        "typography" => { "size" => "expanded" }
-      }
-    })
-
-    assert_in_delta 1.12, app_base_font_size
-  end
-
-  private
-
-  def stub_font_config(font_name)
-    config = {
-      "ui" => {
-        "typography" => {
-          "font" => font_name
-        }
-      }
-    }
-    Boilermaker::Config.instance_variable_set(:@data, config)
+  test "page_title uses content_for title when available" do
+    content_for :title, "Custom Title"
+    title = page_title
+    assert_includes title, "Custom Title"
+    assert_includes title, Boilermaker.config.app_name
   end
 end
